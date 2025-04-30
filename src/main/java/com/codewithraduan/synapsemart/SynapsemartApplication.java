5c2fd40ddc2eab55e1ce7587577bc5d8f1c1cca7
raduansm <raydoansm@gmail.com>
2025-04-30 10:10:36 +0600

    My first commit

---
+++ src/main/java/com/codewithraduan/synapsemart/SynapsemartApplication.java
@@ -0,0 +1,13 @@
+package com.codewithraduan.synapsemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapsemartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapsemartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithraduan/synapsemart/SynapsemartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithraduan.synapsemart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapsemartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
